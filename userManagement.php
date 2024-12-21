4f5af0410b1a1f559ba789b4a49e364ba6728c5f
Zeyad-Tantawy1 <zeyad2202617@miuegypt.edu.eg>
2024-12-21 16:00:53 +0200

    add and delete update

--- userManagement.php
+++ userManagement.php
@@ -1,83 +1,120 @@
 <?php
-include_once('config/connect_DB.php'); // Include the Database singleton
-
-class UserManagement {
-    private static $instance;
+include('config/connect_DB.php');
+class UserManager {
     private $conn;
 
-    private function __construct() {
-        $db = Database::getInstance(); // Get the Database instance
-        $this->conn = $db->getConnection(); // Get the connection
+    public function __construct()
+    {
+        $this->conn = Database::getInstance()->getConnection();
     }
 
-    public static function getInstance() {
-        if (!self::$instance) {
-            self::$instance = new self();
-        }
-        return self::$instance;
+    public function addUser($id, $username, $email, $password)
+    {
+        $stmt = $this->conn->prepare("INSERT INTO users (id, username, email, pass) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("isss", $id, $username, $email, $password);
+        return $stmt->execute();
     }
 
-    public function getUsers() {
-        $stmt = $this->conn->prepare("SELECT * FROM users");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $users = array();
-        while ($row = $result->fetch_assoc()) {
-            $users[] = $row;
+    public function deleteUser($id)
+    {
+        $stmt = $this->conn->prepare("DELETE FROM users WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        return $stmt->execute();
+    }
+
+    public function fetchAllUsers()
+    {
+        $sql = "SELECT * FROM users";
+        $result = $this->conn->query($sql);
+        $users = [];
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $users[] = $row;
+            }
         }
         return $users;
     }
 
-    public function getUser($id) {
-        $stmt = $this->conn->prepare("SELECT * FROM users WHERE id = ?");
-        $stmt->bind_param("i", $id);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        return $result->fetch_assoc();
-    }
-
-    public function createUser($username, $password, $email) {
-        $stmt = $this->conn->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
-        $stmt->bind_param("sss", $username, $password, $email);
-        $stmt->execute();
-        return $stmt->insert_id;
-    }
-
-    public function updateUser($id, $username, $password, $email) {
-        $stmt = $this->conn->prepare("UPDATE users SET username = ?, password = ?, email = ? WHERE id = ?");
-        $stmt->bind_param("sssi", $username, $password, $email, $id);
-        $stmt->execute();
-        return $stmt->affected_rows;
-    }
-
-    public function deleteUser($id) {
-        $stmt = $this->conn->prepare("DELETE FROM users WHERE id = ?");
-        $stmt->bind_param("i", $id);
-        $stmt->execute();
-        return $stmt->affected_rows;
+    public function closeConnection()
+    {
+        $this->conn->close();
     }
 }
-$userManagement = UserManagement::getInstance();
-$users = $userManagement->getUsers();
 
+$userManager = new UserManager();
+$message = '';
+$errors = ['email' => '', 'pass' => '', 'username' => '', 'id' => ''];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['pass'];
+    $id = $_POST['id'];
+
+    if (empty($email)) {
+        $errors['email'] = 'An email is required.';
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = 'Email must be a valid email address.';
+    }
+
+    if (empty($password)) {
+        $errors['pass'] = 'A password is required.';
+    } elseif (!preg_match('/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$/', $password)) {
+        $errors['pass'] = 'Password must contain an uppercase letter, lowercase letter, number, and special character.';
+    }
+
+    if (empty($username)) {
+        $errors['username'] = 'Username is required.';
+    }
+
+    if (empty($id) || !is_numeric($id)) {
+        $errors['id'] = 'ID is required and must be numeric.';
+    }
+
+    if (!array_filter($errors)) {
+        if ($userManager->addUser($id, $username, $email, $password)) {
+            $message = "User added successfully.";
+            header("Location: userManagement.php");
+            exit();
+        } else {
+            $message = "Error adding user.";
+        }
+    }
+}
+
+if (isset($_GET['delete_id'])) {
+    $delete_id = $_GET['delete_id'];
+
+    if ($userManager->deleteUser($delete_id)) {
+        $message = "User deleted successfully.";
+        header("Location: userManagement.php");
+        exit();
+    } else {
+        $message = "Error deleting user.";
+    }
+}
+
+$users = $userManager->fetchAllUsers();
+$userManager->closeConnection();
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Management</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
 <body>
-    <?php include('templates/header.php');?>
-    <div class="container">
-        <?php if (isset($message)) : ?>
-            <p><?php echo $message; ?></p>
-        <?php endif; ?>
-        <?php if ($users): ?>
+<?php include('templates/header.php'); ?>
+
+<div class="container">
+    <?php if (!empty($message)): ?>
+        <p><?php echo $message; ?></p>
+    <?php endif; ?>
+
+    <?php if (!empty($users)): ?>
         <table class="highlight">
             <thead>
                 <tr>
@@ -90,22 +127,27 @@ $users = $userManagement->getUsers();
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($users as $user) : ?>
-                <tr>
-                    <td><?php echo $user['username']; ?></td>
-                    <td><?php echo $user['email']; ?></td>
-                    <td><?php echo $user['pass']; ?></td>
-                    <td><?php echo $user['id']; ?></td>
-                    <td><?php echo $user['role']; ?></td>
-                    <td>
-                        <a href="edit.php?id=<?php echo $user['id']; ?>">Edit</a>
-                        <a href="delete.php?id=<?php echo $user['id']; ?>">Delete</a>
-                    </td>
-                </tr>
+                <?php foreach ($users as $user): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($user['username']); ?></td>
+                        <td><?php echo htmlspecialchars($user['email']); ?></td>
+                        <td><?php echo htmlspecialchars($user['pass']); ?></td>
+                        <td><?php echo htmlspecialchars($user['id']); ?></td>
+                        <td><?php echo htmlspecialchars(strtoupper($user['role'])); ?></td>
+                        <td>
+                            <a href="userManagement.php?delete_id=<?php echo $user['id']; ?>" class="btn brand z-depth-0" onclick="return confirm('Are you sure you want to delete this user?');">Delete</a>
+                            <a href="edit.php?id=<?php echo $user['id']; ?>" class="btn brand z-depth-0">Edit</a>
+                        </td>
+                    </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
-        <?php endif; ?>
-    </div>
+        <a href="SignUp.php" class="btn brand z-depth-0">Add</a>
+    <?php else: ?>
+        <h5 class="red-text">No user at this time</h5>
+    <?php endif; ?>
+</div>
+
+<?php include('templates/footer.php'); ?>
 </body>
 </html>
